05940e5b306686595b98b112c7877571c1171305
Antonio Lobato <lobato@lobato-macbookpro2.roam.corp.google.com>
2019-07-20 14:59:40 -0700

    beam WIP

--- beam/unified-logging/src/main/java/com/google/UnifiedLogging.java
+++ beam/unified-logging/src/main/java/com/google/UnifiedLogging.java
@@ -1,5 +1,15 @@
 package com.google;
 
-public class UnifiedLogging {
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
+public class UnifiedLogging {
+  static String projectName;
+  public static void main(String[] args) {
+    DataflowPipelineOptions options = PipelineOptionsFactory
+    .fromArgs(args)
+    .withValidation()
+    .as(DataflowPipelineOptions.class);
+  projectName = options.getProject();
+  }
 }
