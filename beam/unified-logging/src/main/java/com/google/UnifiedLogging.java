91d7de984f8b5aa332140b4dad14cf777b18f9c4
Antonio Lobato <lobato@lobato-macbookpro2.roam.corp.google.com>
2019-07-21 09:29:59 -0700

    beam WIP

--- beam/unified-logging/src/main/java/com/google/UnifiedLogging.java
+++ beam/unified-logging/src/main/java/com/google/UnifiedLogging.java
@@ -1,15 +1,34 @@
 package com.google;
 
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.values.PCollection;
 
 public class UnifiedLogging {
   static String projectName;
   public static void main(String[] args) {
+    
     DataflowPipelineOptions options = PipelineOptionsFactory
     .fromArgs(args)
     .withValidation()
     .as(DataflowPipelineOptions.class);
-  projectName = options.getProject();
+
+    projectName = options.getProject();
+    
+    String subscription = "projects/"
+    + projectName
+    + "/subscriptions/"
+    + "unified-logging";
+
+    Pipeline p = Pipeline.create(options);
+
+    PCollection<String> pubsubStream = p
+      .apply("Read from Pub/Sub", PubsubIO
+        .readStrings()
+      .fromSubscription(subscription));
+    
+    p.run();
   }
 }
